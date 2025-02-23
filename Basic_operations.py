2ed4a90460123fe5106ac5973d6628b0123ea891
Onyinye  Sarah Azunna <164611688+Onyinye92@users.noreply.github.com>
2025-02-23 21:04:33 +0100

    Add files via upload

---
+++ Basic_operations.py
@@ -0,0 +1,13 @@
+#Define the numbers
+number_1 = 10
+number_2 = 5
+
+#Calculate the numbers
+sum_result = number_1 + number_2
+difference_result = number_1 - number_2
+multiply_result = number_1 * number_2
+
+#Print the result
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", multiply_result)
