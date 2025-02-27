54c3a85f08ed458b6c52f40f6f5a80a3ceb9a246
Onyinye  Sarah Azunna <164611688+Onyinye92@users.noreply.github.com>
2025-02-27 18:02:32 +0100

    Add files via upload

---
+++ Simple_interest.py
@@ -0,0 +1,13 @@
+#Define the variables
+principal =1000       #Initial investment amount in dollars
+rate = 0.5            #Yearly interest rate (5% computed as decimal)
+time = 3              #Time period in years
+
+
+#Calculate the simple interest 
+interest = principal * rate * time
+i = 1000 * 0.05 * 3
+
+
+#Print the simple interest .format ()
+print (f"The simple interest is: {interest}")
